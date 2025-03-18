df513278b4179c1a7d9627bfd46fec8bfafedbdd
knhmochi <natsuki.26a.tb@gmail.com>
2025-03-18 13:42:30 +0900

    before deploy2

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,7 +3,7 @@
 namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
-use Illuminate\Support\Facades\view;
+use Illuminate\Support\Facades\View;
 use Illuminate\Support\Facades\Auth;
 
 class AppServiceProvider extends ServiceProvider
@@ -21,16 +21,15 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        View::composer('*', function ($view) {
-            // ログインしていれば、ユーザーの関連データを取得
-            // hospital が null の場合はデフォルト値「〇〇病院」とする
+       /*  // ビューコンポーザーを使用して、ビューに変数を渡す
+        \Illuminate\Support\Facades\View::composer('*', function ($view) {
             $hospitalName = '〇〇病院';
             $departmentName = '〇〇';
             $user_role = '〇〇〇〇';
     
-            if (Auth::check()) {
+            if (\Illuminate\Support\Facades\Auth::check()) {
                 // ユーザーがログインしている場合
-                $user = Auth::user();
+                $user = \Illuminate\Support\Facedes\Auth::user();
                 if ($user->hospital) {
                     $hospitalName = $user->hospital->name ?? '〇〇病院';
                 }
@@ -45,6 +44,6 @@ class AppServiceProvider extends ServiceProvider
             $view->with('hospitalName', $hospitalName);
             $view->with('departmentName', $departmentName);
             $view->with('user_role', $user_role);
-        });
+        }); */
     }
 }
